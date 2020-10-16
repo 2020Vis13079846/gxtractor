db26ce4c8e22375c21f690e71e8df28c2c9a4989
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-16 11:36:02 +0300

    Create gxtractor.py

---
+++ gxtractor.py
@@ -0,0 +1,31 @@
+import sys
+import re
+
+def help_usage():
+    print("Usage: objdump <input_file> | gxtractor")
+
+def main():
+    if not sys.stdin.isatty():
+        try:
+            shellcode = ""
+            lenght = 0
+            while 1:
+                item = sys.stdin.readline()
+                if item:
+                    if re.match("^[ ]*[0-9a-f]*:.*$",item):
+                        item = item.split(":")[1].lstrip()
+                        x = item.split("\t")
+                        opcode = re.findall("[0-9a-f][0-9a-f]",x[0])
+                        for i in opcode:
+                            shellcode += "\\x" + i
+                            lenght += 1
+                else: 
+                    break
+                print(shellcode)
+                print("Shellcode Lenght: " + str(lenght))
+        except:
+            help_usage()
+    else:
+        help_usage()
+        
+main()
